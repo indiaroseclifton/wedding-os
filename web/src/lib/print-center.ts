abc03d21507017afbcd9be6d517a23ec0702deef
ng549 <ng@moremarginco.com>
2026-09-17 16:06:27 -0400

    Lock Print Center as the print module name

---
+++ web/src/lib/print-center.ts
@@ -0,0 +1,5 @@
+export const PRINT_CENTER = {
+  name: "Print Center",
+  href: "/studio/print-center",
+  line: "Letters, cards, programs, floor sheets, the packet. Home printer tonight. A shop later.",
+} as const;
